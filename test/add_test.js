ed7ec50b8e92b0fbb5154832ea6d8f9b48bbf396
Lauri Viitanen <lauri.viitanen@tuni.fi>
2021-11-03 12:52:40 +0200

    Added tests for division. Renamed files, refactored add_tests.

--- test/add_test.js
+++ test/add_test.js
@@ -9,8 +9,8 @@ let testCases = [
    { a: 0.6, b: 0.2, result: 0.8 },
    { a: '0', b: 1, result: 1 },
    { a: 1, b: 0, result: 1 },
-   { a: 1/0, b: 10, result: Infinity },
-   { a: -1/0, b: 0, result: -Infinity },
+   { a: Infinity, b: 10, result: Infinity },
+   { a: -Infinity, b: 0, result: -Infinity },
    { a: Number.MAX_SAFE_INTEGER, b: 1, result: 9007199254740992n},
    { a: Number.MAX_VALUE, b: 1, result: 1.7976931348623157e+308},
    { a: -Number.MAX_VALUE, b: 1, result: -1.7976931348623157e+308},
@@ -21,29 +21,26 @@ let testCases = [
    { a: Number.MIN_VALUE, b: -1, result: -1},
 ]
 
-// Test all the test cases above
+
 describe('Sum of numbers', () => {
+   // Test all the test cases above
    for (let testCase of testCases) {
       it(`When a is ${testCase.a} and b is ${testCase.b}, the sum a + b should return ${testCase.result}`, () => {
          assert.equal(add(testCase.a, testCase.b), testCase.result);
       });
    }
-});
 
-
-// Test other special cases
-describe('Sum of numbers with special results', () => {
-   it(`When a is -1/0 and b is 1/0, the sum a + b should return NaN`, () => {
-      assert.equal(isNaN(add( -1/0, 1/0)), true);
+   // Test other special cases
+   it(`When a is -Infinity and b is Infinity, the sum a + b should return NaN`, () => {
+      assert.equal(isNaN(add( -Infinity, Infinity)), true);
    });
-   it(`When a NaN and b is 1, the sum a + b should return NaN`, () => {
+   it(`When a is NaN and b is 1, the sum a + b should return NaN`, () => {
       assert.equal(isNaN(add( NaN, 1)), true);
    });
    it(`When a is empty string and b is 1, the sum a + b should return 1`, () => {
       assert.equal(add( "", 1), 1);
    });
-   it(`When a null and b is 1, the sum a + b should return 1`, () => {
+   it(`When a is null and b is 1, the sum a + b should return 1`, () => {
       assert.equal(add( null, 1), 1);
    });
 });
-

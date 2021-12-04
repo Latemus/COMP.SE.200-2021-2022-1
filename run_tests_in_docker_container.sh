ce0bed106729684e58a84ba694e0ecd361b25371
Lauri Viitanen <lauri.viitanen@tuni.fi>
2021-12-04 09:54:11 +0200

    cleaned local file path from sh script

--- run_tests_in_docker_container.sh
+++ run_tests_in_docker_container.sh
@@ -5,7 +5,8 @@ docker run --rm comp.se.200-test-suite \
     --name test_suite_container
 
 # # If we would want to make volume-bind between container and host system
+# # Check working directory and host system specifics
 # docker run --rm comp.se.200-test-suite \
 #     --it \
 #     --name test_suite_container \
-#     --mount type=bind,source=/home/lv/OpinnotLocal/testikurssi/COMP.SE.200-2021-2022-1/coverage,target=/test_suite/coverage sleep 5000
+#     --mount type=bind,source="{pwd}/coverage,target=/test_suite/coverage sleep 5000
